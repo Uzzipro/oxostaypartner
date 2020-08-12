a72575c714f7d9e83560c2823d5e486ab22105ef
PaapiJatt <ubassi15@gmail.com>
2020-08-12 08:50:54 +0530

    first commit

---
+++ app/src/main/java/com/partner/oxostay/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.partner.oxostay.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.partner.oxostay.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
